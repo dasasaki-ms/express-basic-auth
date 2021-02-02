ec9f5c8cc0c939c014bb6032606fc7c0f0656a60
root <root@dasasaki-pc11.localdomain>
2021-02-02 12:30:43 +0900

    authパスでauthenticationできるexpressプロジェクト作成

---
+++ routes/auth/index.js
@@ -0,0 +1,15 @@
+const auth = require('basic-auth');
+
+const admins = {
+	  'username': { password: 'password' },
+};
+
+module.exports = function (request, response, next) {
+	  const user = auth(request);
+	  if (!user || !admins[user.name] || admins[user.name].password !== user.pass) {
+		      response.set('WWW-Authenticate', 'Basic realm="example"');
+		      return response.status(401).send();
+		    }
+          response.render('index', { title: 'Express' });
+	  //return next();
+};
